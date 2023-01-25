102c82e90fc68894b5bc09f69cc0d51720d9f5e3
andreparmar <88522804+andreparmar@users.noreply.github.com>
2023-01-25 18:18:53 -0500

    Add hero and marquee sections to homepage
    This commit adds a hero section and a marquee section to the homepage. The hero section includes a large image, navigation, and dynamic typing. While the marquee section displays my daily workflow apps. Both sections are fully responsive and improve the overall design and user experience of the homepage.
    CSS and JS files have been added and modified accordingly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["src/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      body: ["Satoshi", "system-ui", "sans-serif"],
+      display: ["Clash Display", "system-ui", "sans-serif"],
+    },
+    extend: {
+      animation: {
+        "spin-slow": "spin 20s linear infinite",
+        marquee: "marquee 70s linear infinite",
+        marquee2: "marquee2 70s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      blur: {
+        gradiant: "200px",
+      },
+    },
   },
   plugins: [],
 };
